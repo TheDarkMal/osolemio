ede964317a6d07d6756de864ad9a72d9fe6ca7ca
Mauricio Vergara <mauricio301051020@gmail.com>
2019-08-02 11:43:02 -0500

    categorias en proceso de terminar

---
+++ JavaScript/js.js
@@ -0,0 +1,42 @@
+<style>
+#ab{
+    margin-left: 50px;
+    
+ padding-left: 230px;
+}
+h1{
+text-align: center;
+/* padding-left: 510px;  otra forma de centrar*/
+}
+header{
+
+    background-image: url("imagenes/marron2.jpg");
+    padding-top: 20px;
+}
+ul{
+
+    color: aqua;
+    
+}
+body{
+
+   background-image: url("imagenes/prueba1.jpg")
+}
+#down{
+float: inline-end;
+
+}
+#hola{
+margin: 500px 0px 0px 0px;
+
+
+}
+p{
+    padding-left: 100px;
+    color: black;
+}
+footer{
+
+    background-image: url("imagenes/marron2.jpg");
+}
+</style>
